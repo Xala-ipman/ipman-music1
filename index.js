8c0cc9ead919e035e63b11fc9a455f22e7e7f099
Xala-ipman <89108413+Xala-ipman@users.noreply.github.com>
2021-08-18 02:15:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('');
+client.login('ODQ4MTkwMjgwNTk1MDEzNjcy.YLJAwQ.WdO-NxNWlaJJidemYX_jI0NEQWc');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
