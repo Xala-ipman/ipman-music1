ebefe0c5683e5a458a36758a1d0d6aae9e6fafc4
Xala-ipman <89108413+Xala-ipman@users.noreply.github.com>
2021-08-18 06:29:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ client.on("guildCreate" , DarkMan => {
 });
 
 client.on("guildCreate", guild => {
-  let channel = client.channels.cache.get("829675413470969886");
+  let channel = client.channels.cache.get("862778026164355112");
   let embed = new MessageEmbed().setColor("#146DF6")
   .setAuthor(client.user.username, client.user.avatarURL())
   .setTitle( `✅ Join Server`)
@@ -198,7 +198,7 @@ client.on("guildCreate", guild => {
 });
 
 client.on("guildDelete", guild => {
-  let channel = client.channels.cache.get("829675413470969886");
+  let channel = client.channels.cache.get("862778065854922802");
   let embed = new MessageEmbed()
   .setColor("#146DF6")
   .setAuthor(client.user.username, client.user.avatarURL())
